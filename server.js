3f8ab7e0c42e5b3d1486d900a95ce50467554e3e
Eric Meissner <meissner.eric.7@gmail.com>
2021-08-20 14:31:26 +0100

    First push, Recipe Graph works with ForceAtlas2, basic.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+app.use('/request-type', (req, res, next) => {
+  console.log('Request type: ', req.method);
+  next();
+});
+
+app.use('/public', express.static('public'));
+app.use('/public', serveIndex('public'));
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
