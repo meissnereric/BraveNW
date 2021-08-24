071c0005703c1d1a80a02b600aa66184c418ca1b
Eric Meissner <meissner.eric.7@gmail.com>
2021-08-24 20:13:24 +0100

    Successfully loading Recipe Graph with React Sigma

--- src/Components/ItemGraph.tsx
+++ src/Components/ItemGraph.tsx
@@ -2,7 +2,7 @@ import { settings } from 'cluster';
 import React from 'react';
 import {Sigma, RelativeSize, LoadGEXF, Filter, ForceAtlas2, RandomizeNodePositions} from 'react-sigma';
 
-let myGraph = {nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]};
+// let myGraph = {nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]};
 
 
 class GraphWrapper extends React.Component {
@@ -10,8 +10,27 @@ class GraphWrapper extends React.Component {
         return (
             <div>
 
-                <Sigma graph={myGraph} settings={{drawEdges: true, clone: false}}>
+                {/* <Sigma graph={myGraph} settings={{drawEdges: true, clone: false}}>
                     <RelativeSize initialSize={15}/>
+                    <RandomizeNodePositions/>
+                </Sigma> */}
+                <Sigma
+                    settings={{
+                        batchEdgesDrawing: true,
+                        defaultLabelColor: '#777',
+                        defaultLabelSize: 8,
+                        defaultNodeColor: '#3388AA',
+                        drawEdgeLabels: false,
+                        drawEdges: true,
+                        hoverFontStyle: 'text-size: 11',
+                        labelThreshold: 12
+                    }}
+                    style={{
+                        height: '400px',
+                        maxWidth: 'inherit'
+                    }}
+                    >
+                    <LoadGEXF path="../data/filtered_recipe_graph_8_21_2021.gexf" />
                     <RandomizeNodePositions/>
                 </Sigma>
                 {/* <Sigma  
